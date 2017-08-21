40a041b6fc8083fa0e2b6df63ac95762d445cd9f
Haden Jackson-Robbins <haden.jackson-robbins@dunelm.com>
2017-08-21 14:24:21 +0100

    Remove unnecessary import statement

--- src/test/java/stepDefinitions/StepDefs.java
+++ src/test/java/stepDefinitions/StepDefs.java
@@ -3,12 +3,9 @@ package stepDefinitions;
 import cucumber.api.DataTable;
 import cucumber.api.PendingException;
 import cucumber.api.Scenario;
-import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import org.junit.AfterClass;
-import org.junit.jupiter.api.AfterAll;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
